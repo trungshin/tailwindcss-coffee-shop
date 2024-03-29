3dec80966a2a56f4d072725031bfea014fa26dea
trungshin <trungviet6969@gmail.com>
2024-03-29 10:20:32 +0700

    header_menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-coffee': '#c89f94'
+      }
+    },
   },
   plugins: [],
 }
